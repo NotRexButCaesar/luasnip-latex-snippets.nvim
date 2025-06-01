d2ced91294e9a0f16e9a4e6e6e7e35ebcc20de92
NotRexButCaesar <68972888+NotRexButCaesar@users.noreply.github.com>
2025-06-01 13:24:54 -0700

    Update math_iA.lua

--- lua/luasnip-latex-snippets/math_iA.lua
+++ lua/luasnip-latex-snippets/math_iA.lua
@@ -101,7 +101,7 @@ function M.retrieve(is_math)
       "\\frac{\\mathrm{d}${1:V}}{\\mathrm{d}${2:x}} $0"
     ),
 
-    parse_snippet({ trig = "eol", name = "end of line" }, "\\\\\\\\\\\\n"),
+    parse_snippet({ trig = "eol", name = "end of line" }, "\\\\\\\\\\\n"),
     parse_snippet({ trig = "ergo", name = "therefore" }, "\\therefore"),
     parse_snippet({ trig = "dbox", name = "dashbox" }, "\\dboxed{$1}$0"),
     parse_snippet({ trig = "box", name = "box" }, "\\boxed{$1}$0"),
